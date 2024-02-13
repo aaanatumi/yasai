54594fcb66185ec651c969ecf64ef847d9a53466
natsumi S <neu.begegnung@gmail.com>
2024-02-13 22:07:32 +0900

    model.hennkou

--- yasai.py
+++ yasai.py
@@ -7,9 +7,9 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 
 
-classes = ['きゃべつ','じゃがいも','とまと','たまねぎ','かぼちゃ']
+classes = ['きゃべつ','じゃがいも','とまと','たまねぎ','かぼちゃ']#表示する回答
 #モデルで指定したサイズと同じにする
-image_size = 50
+image_size = 80
 
 
 UPLOAD_FOLDER = "uploads"
@@ -25,17 +25,17 @@ model = load_model('./my_model.h5', compile=False)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
+    if request.method == 'POST':#requestはウェブ上のフォームから送信したデータを扱うための関数。request.method
         if 'file' not in request.files:
             flash('ファイルがありません')
-            return redirect(request.url)
+            return redirect(request.url)#redirect()は引数に与えられたurlに移動する関数
         file = request.files['file']
         if file.filename == '':
             flash('ファイルがありません')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            filename = secure_filename(file.filename)#ファイル名に危険な文字列がある場合に無効化（サニタイズ）する
+            file.save(os.path.join(UPLOAD_FOLDER, filename))#os.path.join()で引数に与えられたパスアップロードされた画像を保存。その保存先をfilepathに格納
             filepath = os.path.join(UPLOAD_FOLDER, filename)
 
             #受け取った画像を読み込み、np形式に変換、#grayscale=False.Trueから変更
@@ -49,9 +49,12 @@ def upload_file():
 
             return render_template("yasai2.html",answer=pred_answer)
 
-    return render_template("yasai2.html",answer="")
+    return render_template("yasai2.html",answer=pred_answer)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
     app.run(host ='0.0.0.0',port = port)
+
+#if __name__ == "__main__":
+#    app.run()
